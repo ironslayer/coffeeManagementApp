eeeab67c5dace6e3dcb88ffac8d519a588e77fa1
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-01 21:27:46 -0400

    Fix typo in signup endpoint path in UserRest

--- src/main/java/com/inn/cafe/rest/UserRest.java
+++ src/main/java/com/inn/cafe/rest/UserRest.java
@@ -10,7 +10,7 @@ import java.util.Map;
 @RequestMapping( path = "/user")
 public interface UserRest {
 
-    @PostMapping( path = "/singup")
+    @PostMapping( path = "/signup")
     public ResponseEntity<String> signUp(@RequestBody( required = true )Map<String, String> requestMap);
 
 }
