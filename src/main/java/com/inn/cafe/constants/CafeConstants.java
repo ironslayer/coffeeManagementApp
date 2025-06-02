e220cb1ead3c17acfd15b6a5854be24be7291b6e
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-01 21:27:27 -0400

    Add error and success message constants to CafeConstants

--- src/main/java/com/inn/cafe/constants/CafeConstants.java
+++ src/main/java/com/inn/cafe/constants/CafeConstants.java
@@ -1,4 +1,12 @@
 package com.inn.cafe.constants;
 
 public class CafeConstants {
+
+    public static final String SOMETHING_WENT_WRONG = "Something Went Error.";
+
+    public static final String INVALID_DATA = "Invalid Data.";
+
+    public static final String EMAIL_ALREADY_EXIST = "Email already exist.";
+
+    public static final String SUCCESSFULLY_REGISTERED = "Successfully Registered.";
 }
