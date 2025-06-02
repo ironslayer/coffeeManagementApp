2b200d9827f5826064964d018aedd0e5378f1a9c
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-01 21:29:33 -0400

    Implement signUp method in UserServiceImpl for user registration

--- src/main/java/com/inn/cafe/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/inn/cafe/serviceImpl/UserServiceImpl.java
@@ -1,8 +1,60 @@
 package com.inn.cafe.serviceImpl;
 
+import com.inn.cafe.constants.CafeConstants;
+import com.inn.cafe.dao.UserDao;
+import com.inn.cafe.pojo.Users;
 import com.inn.cafe.service.UserService;
+import com.inn.cafe.utils.CafeUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+import java.util.Objects;
+
+@Slf4j
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserDao userDao;
+
+    @Override
+    public ResponseEntity<String> signUp(Map<String, String> requestMap) {
+        log.info("Inside signup {}", requestMap);
+        try {
+            if (validateSignUpMap(requestMap)) {
+                Users user = userDao.findByEmailId(requestMap.get("email"));
+                if (Objects.isNull(user)) {
+                    userDao.save(getUserFromMap(requestMap));
+                    return CafeUtils.getResponseEntity(CafeConstants.SUCCESSFULLY_REGISTERED, HttpStatus.OK);
+                } else {
+                    return CafeUtils.getResponseEntity(CafeConstants.EMAIL_ALREADY_EXIST, HttpStatus.BAD_REQUEST);
+                }
+            } else {
+                return CafeUtils.getResponseEntity(CafeConstants.INVALID_DATA, HttpStatus.BAD_REQUEST);
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private boolean validateSignUpMap(Map<String, String> requestMap) {
+        return requestMap.containsKey("name") && requestMap.containsKey("contactNumber") && requestMap.containsKey("email") && requestMap.containsKey("password");
+    }
+
+    private Users getUserFromMap(Map<String, String> requestMap) {
+        Users user = new Users();
+        user.setName(requestMap.get("name"));
+        user.setContactNumber(requestMap.get("contactNumber"));
+        user.setEmail(requestMap.get("email"));
+        user.setPassword(requestMap.get("password"));
+        user.setStatus("false");
+        user.setRole("user");
+
+        return user;
+    }
 }
