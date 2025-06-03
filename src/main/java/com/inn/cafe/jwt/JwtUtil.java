ace09a37e3c39a61452c0506ffd871a8aa671bad
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-03 19:59:50 -0400

    Add JwtUtil class for JWT token management and validation

---
+++ src/main/java/com/inn/cafe/jwt/JwtUtil.java
@@ -0,0 +1,69 @@
+package com.inn.cafe.jwt;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtUtil {
+    private static final String secretKey = "btechdays";
+
+    public String extractUsername( String token ){
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration( String token ){
+        return extractClaims(token, Claims::getExpiration);
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public <T> T extractClaims( String token, Function<Claims, T> claimsResolver ){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+    }
+
+    private Boolean isTokenExpired( String token ) {
+        return extractExpiration( token ).before( new Date() );
+    }
+
+    private String createToken (Map<String, Object> claims, String subject){
+        return Jwts
+                .builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + TOKEN_EXPIRATION))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails ){
+        final String username = extractUsername( token );
+        return ( username.equals( userDetails.getUsername() ) && !isTokenExpired( token ) );
+    }
+
+
+}
