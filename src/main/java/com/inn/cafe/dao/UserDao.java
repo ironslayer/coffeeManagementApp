9394a93ceebfeaa8baa7247b27838d405aeada02
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-06 19:46:52 -0400

    Add methods to UserDao for user retrieval and status updates

--- src/main/java/com/inn/cafe/dao/UserDao.java
+++ src/main/java/com/inn/cafe/dao/UserDao.java
@@ -1,12 +1,22 @@
 package com.inn.cafe.dao;
 
 import com.inn.cafe.pojo.Users;
+import com.inn.cafe.wrapper.UserWrapper;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserDao extends JpaRepository<Users, Integer> {
 
     Users findByEmailId(@Param("email") String email);
 
+    List<UserWrapper> getAllUsers();
+
+    @Transactional
+    @Modifying
+    Integer updateStatus( @Param( "status" ) String status, @Param( "id" ) Integer id );
 
 }
