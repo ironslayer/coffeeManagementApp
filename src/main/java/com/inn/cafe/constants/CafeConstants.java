2c3c13cab696b0ab8c9401630050b4cabbc36fa9
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-06 19:46:43 -0400

    Add new constants for unauthorized access and user status updates in CafeConstants

--- src/main/java/com/inn/cafe/constants/CafeConstants.java
+++ src/main/java/com/inn/cafe/constants/CafeConstants.java
@@ -9,4 +9,10 @@ public class CafeConstants {
     public static final String EMAIL_ALREADY_EXIST = "Email already exist.";
 
     public static final String SUCCESSFULLY_REGISTERED = "Successfully Registered.";
+
+    public static final String UNAUTHORIZED_ACCESS = "Unauthorized access.";
+
+    public static final String USER_ID_DOES_NOT_EXIST = "User ID does not exist.";
+
+    public static final String USER_UPDATED_SUCCESSFULLY = "User Status Updated Successfully.";
 }
