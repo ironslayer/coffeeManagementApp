9cf07de6209535fb39d98b14b94b1651ddb6dfcb
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-08 05:08:33 -0400

    Add forgot password method in UserService

--- src/main/java/com/inn/cafe/service/UserService.java
+++ src/main/java/com/inn/cafe/service/UserService.java
@@ -20,4 +20,7 @@ public interface UserService {
 
     ResponseEntity<String> changePassword( Map<String, String> requestMap );
 
+    ResponseEntity<String> forgotPassword( Map<String, String> requestMap );
+
+
 }
