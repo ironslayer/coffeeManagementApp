8f9eb8e7e1d43ac41174a92379578c101beb9663
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-08 20:45:31 -0400

    Add PasswordResetTokenRepository for managing password reset tokens

---
+++ src/main/java/com/inn/cafe/dao/PasswordResetTokenRepository.java
@@ -0,0 +1,16 @@
+package com.inn.cafe.dao;
+//REVISAR -------------------------------------------------------------------------------------------------------------------
+
+import com.inn.cafe.pojo.PasswordResetToken;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+    PasswordResetToken findByToken(String token);
+
+    @Modifying
+    @Transactional
+    void deleteByToken(String token);
+}
+//-------------------------------------------------------------------------------------------------------------------
