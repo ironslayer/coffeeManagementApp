2cbb480c579035a64bc5379a4ac59ff2c08b2206
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-10 05:11:58 -0400

    Implement getAllCategory method in CategoryServiceImpl for conditional category retrieval

--- src/main/java/com/inn/cafe/serviceImpl/CategoryServiceImpl.java
+++ src/main/java/com/inn/cafe/serviceImpl/CategoryServiceImpl.java
@@ -1,18 +1,23 @@
 package com.inn.cafe.serviceImpl;
 
+import com.google.common.base.Strings;
 import com.inn.cafe.constants.CafeConstants;
 import com.inn.cafe.dao.CaregoryDao;
 import com.inn.cafe.jwt.JwtFilter;
 import com.inn.cafe.pojo.Category;
 import com.inn.cafe.service.CategoryService;
 import com.inn.cafe.utils.CafeUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
@@ -58,6 +63,20 @@ public class CategoryServiceImpl implements CategoryService {
         }
         category.setName(requestMap.get( "name" ));
         return category;
+    }
+
+    @Override
+    public ResponseEntity<List<Category>> getAllCategory(String filterValue) {
+        try {
+            if ( !Strings.isNullOrEmpty( filterValue ) && filterValue.equalsIgnoreCase( "true" ) ){
+                log.info( "Inside if" );
+                return new ResponseEntity<List<Category>>( caregoryDao.getAllCategory(), HttpStatus.OK );
+            }
+            return new ResponseEntity<>(caregoryDao.findAll(), HttpStatus.OK);
+        } catch ( Exception ex ){
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<List<Category>>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
