d3078bdb155fb03268f1e0028268a386c060b297
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-10 04:30:25 -0400

    Add Category entity for category management

---
+++ src/main/java/com/inn/cafe/pojo/Category.java
@@ -0,0 +1,36 @@
+package com.inn.cafe.pojo;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@NamedQuery( name = "Category.getAllCategory", query = "select c from Category c ")
+
+@Data
+@Entity
+@DynamicUpdate
+@DynamicInsert
+@Table(name = "category")
+public class Category implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    @Column( name = "id" )
+//    @Setter( AccessLevel.NONE )
+    private Integer id;
+
+    @Column( name = "name" )
+    private String name;
+
+
+
+}
