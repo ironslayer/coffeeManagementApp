a2f108343f99e4cbebe019b426aaf2d6ff1d8caf
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-10 06:35:20 -0400

    Remove unused setter access level annotation from Category entity

--- src/main/java/com/inn/cafe/pojo/Category.java
+++ src/main/java/com/inn/cafe/pojo/Category.java
@@ -25,7 +25,6 @@ public class Category implements Serializable {
     @Id
     @GeneratedValue( strategy = GenerationType.IDENTITY )
     @Column( name = "id" )
-//    @Setter( AccessLevel.NONE )
     private Integer id;
 
     @Column( name = "name" )
