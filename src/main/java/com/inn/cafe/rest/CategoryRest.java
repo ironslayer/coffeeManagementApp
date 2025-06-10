de8ceaec9a37e7e0958ad0afe4f6d69da4eb8872
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-10 04:30:29 -0400

    Add CategoryRest interface for category management endpoints

---
+++ src/main/java/com/inn/cafe/rest/CategoryRest.java
@@ -0,0 +1,20 @@
+package com.inn.cafe.rest;
+
+import com.inn.cafe.pojo.Category;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RequestMapping( path = "/category")
+public interface CategoryRest {
+
+    @PostMapping( path = "/add")
+    ResponseEntity<String> addNewCategory( @RequestBody( required = true) Map<String, String> mapRequest );
+
+//    @GetMapping( path = "/get")
+//    public ResponseEntity<List<Category>> getAllCategory(@RequestParam( required = false ) String filterValue );
+//
+
+}
