d2814f6481a0f4c23e22b4e30807e2501637a3e3
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-11 03:18:21 -0400

    Add NamedQuery for retrieving all products with specific fields

--- src/main/java/com/inn/cafe/pojo/Product.java
+++ src/main/java/com/inn/cafe/pojo/Product.java
@@ -8,6 +8,8 @@ import org.hibernate.annotations.DynamicUpdate;
 import java.io.Serial;
 import java.io.Serializable;
 
+@NamedQuery( name = "Product.getAllProduct", query = "select new com.inn.cafe.wrapper.ProductWrapper(p.id, p.name, p.description, p.price, p.status, p.category.id, p.category.name ) from Product p  ")
+
 @Data
 @Entity
 @DynamicUpdate
