3c364bcd06ef5259ae760b62011a5c42951bc6d9
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-11 20:46:08 -0400

    Add methods for retrieving products by category and ID in ProductRestImpl

--- src/main/java/com/inn/cafe/restImpl/ProductRestImpl.java
+++ src/main/java/com/inn/cafe/restImpl/ProductRestImpl.java
@@ -69,4 +69,24 @@ public class ProductRestImpl implements ProductRest {
         }
         return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    @Override
+    public ResponseEntity<List<ProductWrapper>> getByCategory(Integer id) {
+        try {
+            return productService.getByCategory( id );
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<>( new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR );
+    }
+
+    @Override
+    public ResponseEntity<ProductWrapper> getById(Integer id) {
+        try {
+            return productService.getById( id );
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<>( new ProductWrapper(), HttpStatus.INTERNAL_SERVER_ERROR );
+    }
 }
