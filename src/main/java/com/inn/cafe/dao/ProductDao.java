59b6c1a02d4c7697c6cc23390d00edeb073a3fa9
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-11 20:02:30 -0400

    Add updateProductStatus method to ProductDao for modifying product status

--- src/main/java/com/inn/cafe/dao/ProductDao.java
+++ src/main/java/com/inn/cafe/dao/ProductDao.java
@@ -2,11 +2,18 @@ package com.inn.cafe.dao;
 
 import com.inn.cafe.pojo.Product;
 import com.inn.cafe.wrapper.ProductWrapper;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ProductDao extends JpaRepository<Product, Integer> {
 
     List<ProductWrapper> getAllProduct();
+
+    @Modifying
+    @Transactional
+    void updateProductStatus(@Param( "status" ) String status, @Param( "id" ) Integer id);
 }
