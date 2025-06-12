5ab9dde9434dd6e60b0df635fd40f3cb1f1f3350
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-11 20:46:13 -0400

    Add methods for retrieving products by category and ID in ProductServiceImpl

--- src/main/java/com/inn/cafe/serviceImpl/ProductServiceImpl.java
+++ src/main/java/com/inn/cafe/serviceImpl/ProductServiceImpl.java
@@ -157,4 +157,24 @@ public class ProductServiceImpl implements ProductService {
         return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @Override
+    public ResponseEntity<List<ProductWrapper>> getByCategory(Integer id) {
+        try {
+            return new ResponseEntity<>(productDao.getProductByCategory( id ), HttpStatus.OK);
+        } catch ( Exception ex ){
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<ProductWrapper> getById(Integer id) {
+        try {
+            return new ResponseEntity<>(productDao.getProductById( id ), HttpStatus.OK);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<>( new ProductWrapper(), HttpStatus.INTERNAL_SERVER_ERROR );
+    }
+
 }
