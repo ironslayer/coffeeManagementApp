17fc2aab6541c40062947554bf4747a46bb9646a
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-11 20:46:04 -0400

    Add methods for retrieving products by category and ID in ProductRest

--- src/main/java/com/inn/cafe/rest/ProductRest.java
+++ src/main/java/com/inn/cafe/rest/ProductRest.java
@@ -11,17 +11,23 @@ import java.util.Map;
 public interface ProductRest {
 
     @PostMapping( path = "/add")
-    ResponseEntity<String> addNewProduct(@RequestBody Map<String, String> requestMap );
+    ResponseEntity<String> addNewProduct( @RequestBody Map<String, String> requestMap );
 
     @GetMapping (path = "/get")
     ResponseEntity<List<ProductWrapper>> getAllProduct();
 
     @PostMapping( path = "/update")
-    ResponseEntity<String> updateProduct(@RequestBody Map<String, String> requestMap );
+    ResponseEntity<String> updateProduct( @RequestBody Map<String, String> requestMap );
 
     @PostMapping( path = "/delete/{id}")
-    ResponseEntity<String> deleteProduct(@PathVariable Integer id );
+    ResponseEntity<String> deleteProduct( @PathVariable Integer id );
 
     @PostMapping( path = "/updateStatus")
-    ResponseEntity<String> updateStatus(@RequestBody Map<String, String> requestMap );
+    ResponseEntity<String> updateStatus( @RequestBody Map<String, String> requestMap );
+
+    @GetMapping (path = "/getByCategory/{id}")
+    ResponseEntity<List<ProductWrapper>> getByCategory( @PathVariable Integer id );
+
+    @GetMapping (path = "/getById/{id}")
+    ResponseEntity<ProductWrapper> getById( @PathVariable Integer id );
 }
