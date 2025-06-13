94d24fcbdb89780e8be4495afe4a652f91501202
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-13 03:03:36 -0400

    Add method to BillService for retrieving PDF files

--- src/main/java/com/inn/cafe/serviceImpl/BillServiceImpl.java
+++ src/main/java/com/inn/cafe/serviceImpl/BillServiceImpl.java
@@ -12,13 +12,17 @@ import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.pdfbox.io.IOUtils;
 import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -178,8 +182,39 @@ public class BillServiceImpl implements BillService {
         } else {
             list = billDao.getBillByUserName( jwtFilter.getCurrentUser() );
         }
-
         return new ResponseEntity<>( list, HttpStatus.OK );
+    }
+
+    @Override
+    public ResponseEntity<byte[]> getPdf(Map<String, Object> requestMap) {
+        log.info( "Inside getPdf: requestMap {}",  requestMap);
+        try {
+            byte[] byteArray = new byte[0];
+            if ( !requestMap.containsKey( "uuid" ) && validateRequestMap( requestMap ) ){
+                return new ResponseEntity<>( byteArray, HttpStatus.OK );
+            }
+            String filePath = CafeConstants.STORE_LOCATION + "/" + (String) requestMap.get( "uuid" ) + ".pdf";
+            if ( CafeUtils.isFileExist( filePath ) ){
+                byteArray = getByteArray( filePath );
+                return new ResponseEntity<>( byteArray, HttpStatus.OK );
+            } else {
+              requestMap.put( "isGenerate", false );
+              generateReport( requestMap );
+              byteArray = getByteArray( filePath );
+              return  new ResponseEntity<>( byteArray, HttpStatus.OK );
+            }
+        } catch ( Exception ex ) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    private byte[] getByteArray(String filePath) throws Exception {
+        File initialFile = new File( filePath );
+        InputStream targetStream = new FileInputStream( initialFile );
+        byte[] byteArray = IOUtils.toByteArray( targetStream );
+        targetStream.close();
+        return byteArray;
     }
 
 }
